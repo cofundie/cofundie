0217e035bffb2b0fe7d154b7c408615cf0f4155d
Victor Yunusa <victoryunusa17@gmail.com>
2019-03-04 16:21:46 +0000

    Added a new hoe it works section to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -80,4 +80,64 @@
 </div>
 <!-- End Front for -->
 
+<!-- What We Do Section -->
+<div id="whatWeDoSection" class="container space-2 space-md-3">
+    <div class="row justify-content-lg-between">
+        <div class="col-lg-4 mb-7 mb-lg-0">
+            <!-- Title -->
+            <small class="text-secondary text-uppercase font-weight-medium mb-2">How it Works?</small>
+            <h3 class="font-weight-medium">More control and greater overview from simple, powerful features</h3>
+            <p>This is where we really begin to visualize your napkin sketches and make them into beautiful pixels.</p>
+            <!-- End Title -->
+        </div>
+
+        <div class="col-lg-7">
+            <div class="row">
+                <div class="col-sm-6 mb-3">
+                    <!-- Icon Blocks -->
+                    <figure id="icon26" class="svg-preloader ie-height-56 w-100 max-width-8 mb-3">
+                        <img class="js-svg-injector" src="{{ asset('assets/svg/icon-26.svg')}}" alt="SVG" data-parent="#icon26">
+                    </figure>
+                    <h4 class="h5">Marketing</h4>
+                    <p class="font-size-1">Front is an incredibly beautiful, fully responsive, and mobile-first projects on the web.</p>
+                    <!-- End Icon Blocks -->
+                </div>
+
+                <div class="col-sm-6 mb-3">
+                    <!-- Icon Blocks -->
+                    <figure id="icon27" class="svg-preloader ie-height-56 w-100 max-width-8 mb-3">
+                        <img class="js-svg-injector" src="{{ asset('assets/svg/icon-27.svg')}}" alt="SVG" data-parent="#icon27">
+                    </figure>
+                    <h4 class="h5">Clean design</h4>
+                    <p class="font-size-1">Front template can be easily customized with its cutting-edge components and features.</p>
+                    <!-- End Icon Blocks -->
+                </div>
+
+                <div class="w-100"></div>
+
+                <div class="col-sm-6 mb-3 mb-sm-0">
+                    <!-- Icon Blocks -->
+                    <figure id="icon25" class="svg-preloader ie-height-56 w-100 max-width-8 mb-3">
+                        <img class="js-svg-injector" src="{{ asset('assets/svg/icon-25.svg')}}" alt="SVG" data-parent="#icon25">
+                    </figure>
+                    <h4 class="h5">Social services <span class="badge badge-success font-weight-medium badge-pill ml-1">beta</span></h4>
+                    <p class="font-size-1">Front is not only for developers but also for designers, and includes a Sketch file.</p>
+                    <!-- End Icon Blocks -->
+                </div>
+
+                <div class="col-sm-6">
+                    <!-- Icon Blocks -->
+                    <figure id="icon28" class="svg-preloader ie-height-56 w-100 max-width-8 mb-3">
+                        <img class="js-svg-injector" src="{{ asset('assets/svg/icon-28.svg')}}" alt="SVG" data-parent="#icon28">
+                    </figure>
+                    <h4 class="h5">Creative ideas</h4>
+                    <p class="font-size-1">Every component and plugin is well documented with live examples.</p>
+                    <!-- End Icon Blocks -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End What We Do Section -->
+
 @endsection
