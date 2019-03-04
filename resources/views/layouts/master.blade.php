d8ed7a0bf4441ec098088d71360578511b023b4b
Victor Yunusa <victoryunusa17@gmail.com>
2019-03-04 15:06:11 +0000

    Removed langauge nav

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -41,32 +41,7 @@
             <!-- Topbar -->
 			<div class="container u-header__hide-content pt-2">
 				<div class="d-flex align-items-center">
-				<!-- Language -->
-				<div class="position-relative">
-					<a id="languageDropdownInvoker" class="dropdown-nav-link dropdown-toggle d-flex align-items-center" href="javascript:;" role="button"
-					aria-controls="languageDropdown"
-					aria-haspopup="true"
-					aria-expanded="false"
-					data-unfold-event="hover"
-					data-unfold-target="#languageDropdown"
-					data-unfold-type="css-animation"
-					data-unfold-duration="300"
-					data-unfold-delay="300"
-					data-unfold-hide-on-scroll="true"
-					data-unfold-animation-in="slideInUp"
-					data-unfold-animation-out="fadeOut">
-					<img class="dropdown-item-icon" src="../../assets/vendor/flag-icon-css/flags/4x3/us.svg" alt="SVG">
-					<span class="d-inline-block d-sm-none">US</span>
-					<span class="d-none d-sm-inline-block">United States</span>
-					</a>
-
-					<div id="languageDropdown" class="dropdown-menu dropdown-unfold" aria-labelledby="languageDropdownInvoker">
-					<a class="dropdown-item active" href="#">English</a>
-					<a class="dropdown-item" href="#">Deutsch</a>
-					<a class="dropdown-item" href="#">Español‎</a>
-					</div>
-				</div>
-				<!-- End Language -->
+			
 
 				<div class="ml-auto">
 					<!-- Jump To -->
