7d4b397166df46b1667aebec01dbe00570799eea
Victor Yunusa <victoryunusa17@gmail.com>
2019-03-04 13:49:56 +0000

    Added footer logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -239,14 +239,8 @@
         <div class="row justify-content-between align-items-center space-1">
             <div class="col-5">
                 <!-- Logo -->
-                <a class="d-inline-flex align-items-center mb-3" href="../home/index.html" aria-label="Front">
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="36px" height="36px" viewBox="0 0 46 46" xml:space="preserve" style="margin-bottom: 0;">
-                        <path fill="#E1E4EA" opacity=".65" d="M23,41.1L23,41.1c-9.9,0-18-8-18-18l0,0c0-9.9,8-18,18-18h11.3c3.7,0,6.6,3,6.6,6.6v11.4C41,33,32.9,41.1,23,41.1z" />
-                        <path class="fill-white" opacity=".5" d="M28,36L28,36c-9.9,0-18-8-18-18l0,0c0-9.9,8-18,18-18h11.3C43,0.1,46,3.1,46,6.7v11.4C46,28,38,36,28,36z" />
-                        <path class="fill-white" opacity=".7" d="M18,46.1L18,46.1c-10,0-18-8-18-18l0,0c0-9.9,8-18,18-18h11.3c3.7,0,6.6,3,6.6,6.6v11.4C35.9,38.1,27.9,46.1,18,46.1z" />
-                        <path class="fill-primary" d="M17.4,34.1V18.4h10.2v2.9h-6.4v3.4H26v2.9h-4.8v6.5H17.4z" />
-                    </svg>
-                    <span class="brand brand-light">Front</span>
+                <a class="d-inline-flex align-items-center mb-3" href="{{url('/')}}" aria-label="Front">
+					<img class="u-header__navbar-brand-default" height="50" src="{{ asset('assets/img/cofundie-white.png')}}" alt="Logo">
                 </a>
                 <!-- End Logo -->
             </div>
