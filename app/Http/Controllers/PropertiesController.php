b7b3c7348f8543cca6e358139dff04e9c65eb696
Victor Yunusa <victoryunusa17@gmail.com>
2019-03-05 10:06:10 +0000

    Removed dummy property content and replaced with dynamic content from database

--- app/Http/Controllers/PropertiesController.php
+++ app/Http/Controllers/PropertiesController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Hash;
+use Session;
+use Validator;
+use Illuminate\Support\Facades\Input;
+use DB;
+use Auth;
 
 class PropertiesController extends Controller
 {
     //
     public function listing()
     {
+        $data['properties'] = DB::table('properties')->where('status','=', '1')->paginate(6);
         
-        return view('pages.listing');
+        return view('pages.listing', $data);
     }
 }
