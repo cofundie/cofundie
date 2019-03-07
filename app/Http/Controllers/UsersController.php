77559821af79b874ed99a7746bc805309f8a3f2f
Victor Yunusa <victoryunusa17@gmail.com>
2019-03-07 11:22:46 +0000

    Added verification middleware to UsersController

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,7 +6,16 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('verified');
+    }
 
     public function dashboard()
     {
